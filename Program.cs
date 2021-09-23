7e4fe6767c6b28b06edb4770c4cc2fcace0ca5f3
Admin <praniknikose@gmail.com>
2021-09-23 16:20:11 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_1_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hii Pranik");
+        }
+    }
+}
